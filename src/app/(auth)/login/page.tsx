130246e65263d05b7af103b786f9793bf4a8fe40
vinimatheus <111582437+vinimatheus@users.noreply.github.com>
2025-05-12 14:16:21 -0300

    feat: adicionar redirecionamento para organizações se o usuário já estiver autenticado

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,14 @@
+import { auth } from "@/auth";
 import { LoginButton } from "@/components/LoginButton";
+import { redirect } from "next/navigation";
+
 export default async function Home() {
+	const session = await auth();
+
+
+  if (session?.user?.id) {
+		redirect("/organizations");
+  }
   return (
     <div className="w-full h-full flex justify-center items-center flex-col gap-y-4">
       <LoginButton />
