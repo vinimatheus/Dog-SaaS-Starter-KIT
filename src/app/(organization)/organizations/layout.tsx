ad5f51bd7fb17a0b8dc113c8ca789e7bdcdf902a
vinimatheus <111582437+vinimatheus@users.noreply.github.com>
2025-05-12 14:08:15 -0300

    feat: remover verificação de organização no layout protegido

--- src/app/(organization)/organizations/layout.tsx
+++ src/app/(organization)/organizations/layout.tsx
@@ -38,10 +38,6 @@ export default async function ProtectedLayout({
 		redirect("/login");
 	}
 
-	if (user.User_Organization.length === 0) {
-		redirect("/organizations");
-	}
-
 	return (
 		<div className="flex-1 space-y-4 p-8 pt-6">
 			{children}
